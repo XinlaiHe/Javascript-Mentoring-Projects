3ce8c25e8b81f1ec507f00359c503a8a0fd2ffbc
Xinlai He <Xinlai_He@epam.com>
2016-05-26 17:46:53 +0800

    more coding

--- ThirdProject/index.js
+++ ThirdProject/index.js
@@ -7,3 +7,23 @@ express.listen(8080, function(){
 
   console.log("hello world");
 });
+
+express.get('/lists', function(req, res){
+
+    res.send("hi, list");
+})
+
+express.get('/lists/:id', function(req, res){
+
+    res.send("hi, list id : " + req.params.id);
+})
+
+express.get('/users', function(req, res){
+
+    res.send("hi, users");
+})
+
+express.get('/users/:id', function(req, res){
+
+    res.send("hi, users id : " + req.params.id);
+})
--- ThirdProject/src/CustomExpress.js
+++ ThirdProject/src/CustomExpress.js
@@ -9,23 +9,74 @@ class CustomExpress{
 
   }
 
-  use(){
+  use(middleware){
 
   }
 
-  get(){
+  get(url, handler){
+
+    this.server.on('request', (request, response) => {
+
+
+      if(request.method == 'GET' && url.includes('/:')){
+
+        let a = request.url.split('/').splice(1);
+        let b = url.replace(/:/g, "").split('/').splice(1);
+
+        if(a.length == b.length && a[0] == b[0]){
+
+          let _params = {};
+          for(let i = 0; i < a.length; i++){
+
+            _params[b[i]] = a[i];
+
+          }
+          request.params = _params;
+          response.send = (str) => {
+
+            response.write(str);
+            response.end();
+          };
+
+          handler(request, response);
+
+        }else{
+
+          // response.statusCode = 404;
+          // response.end();
+          //this.badRequest(response);
+        }
+
+      }else if(request.method == 'GET' && request.url == url){
+
+        request.params = {};
+        response.send = (str) => {
+
+          response.write(str);
+          response.end();
+        };
+
+        handler(request, response);
+
+      }else{
+
+        //this.badRequest(response);
+        // response.statusCode = 404;
+        // response.end();
+      }
+
+    });
+  }
+
+  post(url, handler){
 
   }
 
-  post(){
+  put(url, handler){
 
   }
 
-  put(){
-
-  }
-
-  delete(){
+  delete(url, handler){
 
   }
 
@@ -33,6 +84,15 @@ class CustomExpress{
 
     this.server.listen(port);
     callback();
+  }
+
+  private badRequest(res){
+
+    res.write("Page Not Found");
+    res.end();
+  }
+  private checkMatch(a, b){
+
   }
 }
 
