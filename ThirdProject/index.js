5b8c13571ee54a7a85d41eaf52bcaf2aa56418e7
Xinlai He <Xinlai_He@epam.com>
2016-05-27 09:50:38 +0800

    routing

--- ThirdProject/index.js
+++ ThirdProject/index.js
@@ -1,29 +1,34 @@
 'use strict';
 const Express = require('./src/CustomExpress.js');
-
+​
 let express = new Express();
-
+​
 express.listen(8080, function(){
-
-  console.log("hello world");
+​
+  console.log("Server starts at " + 8080);
 });
-
+​
+express.get('/', function(req, res){
+​
+  res.send("Home page");
+})
+​
 express.get('/lists', function(req, res){
-
-    res.send("hi, list");
+​
+    res.send("hi, this lists page");
 })
-
+​
 express.get('/lists/:id', function(req, res){
-
-    res.send("hi, list id : " + req.params.id);
+​
+    res.send("hi, lists id : " + req.params.id);
 })
-
+​
 express.get('/users', function(req, res){
-
-    res.send("hi, users");
+​
+    res.send("hi, this is users page");
 })
-
+​
 express.get('/users/:id', function(req, res){
-
+​
     res.send("hi, users id : " + req.params.id);
 })
--- ThirdProject/src/CustomExpress.js
+++ ThirdProject/src/CustomExpress.js
@@ -1,99 +1,131 @@
 'use strict';
 const http = require('http');
-
+​
 class CustomExpress{
-
+​
   constructor(){
-
+​
     this.server = http.createServer();
-
+    this.routes = [];
   }
-
+​
   use(middleware){
-
+​
   }
-
+​
   get(url, handler){
+​
+    if(this.routes.indexOf(url) > 0){
+​
+      return;
+​
+    }else{
 
-    this.server.on('request', (request, response) => {
-
-
-      if(request.method == 'GET' && url.includes('/:')){
-
-        let a = request.url.split('/').splice(1);
-        let b = url.replace(/:/g, "").split('/').splice(1);
-
-        if(a.length == b.length && a[0] == b[0]){
-
-          let _params = {};
-          for(let i = 0; i < a.length; i++){
-
-            _params[b[i]] = a[i];
-
+      this.routes.push(url);
+      this.server.on('request', (request, response) => {
+​
+​
+        if(request.method == 'GET' && url.includes('/:')){
+​
+          if(this.checkMatch(request.url, url)){
+​
+            let a = request.url.split('/').splice(1);
+            let b = url.replace(/:/g, "").split('/').splice(1);
+            let _params = {};
+​
+            for(let i = 0; i < a.length; i++){
+​
+              _params[b[i]] = a[i];
+​
+            }
+            request.params = _params;
+            response.send = (str) => {
+​
+              response.write(str);
+              response.end();
+            };
+​
+            handler(request, response);
+​
+          }else{
+​
+            // response.statusCode = 404;
+            // response.end();
+            // this.badRequest(response);
           }
-          request.params = _params;
+​
+        }else if(request.method == 'GET' && request.url == url){
+​
+          request.params = {};
           response.send = (str) => {
-
+​
             response.write(str);
             response.end();
           };
-
+​
           handler(request, response);
-
+​
         }else{
-
+​
+          // this.badRequest(response);
           // response.statusCode = 404;
           // response.end();
-          //this.badRequest(response);
         }
-
-      }else if(request.method == 'GET' && request.url == url){
-
-        request.params = {};
-        response.send = (str) => {
-
-          response.write(str);
-          response.end();
-        };
-
-        handler(request, response);
-
-      }else{
-
-        //this.badRequest(response);
-        // response.statusCode = 404;
-        // response.end();
-      }
-
-    });
+​
+      });
+    }
   }
-
+​
   post(url, handler){
-
+​
   }
-
+​
   put(url, handler){
-
+​
   }
-
+​
   delete(url, handler){
-
+​
   }
-
+​
   listen(port, callback) {
-
+​
     this.server.listen(port);
     callback();
   }
-
-  private badRequest(res){
-
+​
+  badRequest(res){
+​
     res.write("Page Not Found");
     res.end();
+​
   }
-  private checkMatch(a, b){
-
+​
+  checkMatch(requestUrl, Url){
+​
+    let r = requestUrl.split('/').splice(1);
+    let u = Url.split('/').splice(1);
+​
+    if(r.length == u.length){
+​
+      for(let i = 0; i < r.length; i++){
+​
+        if(!u[i].includes(':') && r[i] != u[i]){
+​
+          return false;
+        }
+​
+      }
+​
+      return true;
+​
+    }else{
+​
+      return false;
+    }
+​
   }
 }
-
+​
 module.exports = CustomExpress;
+Add Comment
