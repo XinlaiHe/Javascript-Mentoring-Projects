4da76303444aea03e67a7eba52b70d3066c3820f
XinlaiHe <xinlai_he@epam.com>
2016-06-18 21:28:28 +0800

    modify server

--- AuthProject/server/app.js
+++ AuthProject/server/app.js
@@ -37,22 +37,6 @@ app.use(function(req, res, next) {
 });
 
 app.use('/users', users);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 app.listen(8000, function(err){
   if(err) console.log(err);
